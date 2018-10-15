20bb066d0dd502407b8ff2ad13c362ea3af797dd
David <david.hany129@gmail.com>
2018-10-15 21:13:59 +0200

    Add Tubes

--- core/src/com/starhood/game/states/PlayState.java
+++ core/src/com/starhood/game/states/PlayState.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.starhood.game.FlabbyBirdDemo;
 import com.starhood.game.sprites.Bird;
+import com.starhood.game.sprites.Tube;
 
 /**
  * Created by starhood on 15/10/18.
@@ -13,12 +14,14 @@ import com.starhood.game.sprites.Bird;
 public class PlayState extends State {
 
     private Bird bird;
+    private Tube tube;
     private Texture bg;
 
     public PlayState(GameStateManger gsm) {
         super(gsm);
         bg=new Texture("background.png");
         bird=new Bird(50,200);
+        tube=new Tube(100);
         cam.setToOrtho(false, FlabbyBirdDemo.WIDTH/2,FlabbyBirdDemo.HEIGHT/2);
 
     }
@@ -41,6 +44,8 @@ public class PlayState extends State {
         sb.begin();
         sb.draw(bg,cam.position.x -(cam.viewportWidth/2),0);
         sb.draw(bird.getBird(), bird.getPosition().x,bird.getPosition().y);
+        sb.draw(tube.getTopTube(),tube.getPosTopTube().x,tube.getPosTopTube().y);
+        sb.draw(tube.getBotTube(),tube.getPosBotTube().x,tube.getPosBotTube().y);
         sb.end();
     }
 
